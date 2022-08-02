0ce7dafb11af0608086917b5a9313cad75da0d64
Kyle Sabo <robo210@gmail.com>
2022-08-02 04:30:01 -0700

    Remove clap

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,9 @@
 use ::rsrc::*;
 
 fn main() -> Result<(), rsrc::PEError> {
-    let matches = clap::App::new("rsrc")
-        .arg(
-            clap::Arg::with_name("verbose")
-                .short("v")
-                .long("verbose")
-                .multiple(true)
-                .help("log verbose messages"),
-        )
-        .arg(
-            clap::Arg::with_name("input")
-                .required(true)
-                .index(1)
-                .help("path to input PE file"),
-        )
-        .get_matches();
+    let filename = std::env::args().nth(1).expect("missing argument 1: path to input PE file");
 
-    let filename = matches.value_of("input").unwrap();
-
-    let resources = rsrc::find_resource_directory_from_pe(filename)?;
+    let resources = rsrc::find_resource_directory_from_pe(&filename)?;
 
     let pmres_data2 = resources.find(&"WEVT_TEMPLATE", &"#1")?;
     // let pmres_resource_data = pmres_data2.ok_or(rsrc::PEError::NoResourceTable())?;
